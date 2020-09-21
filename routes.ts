caa61ca50ff047cfd13a5092160716e3821e4ec9
Guilherme Costa da Silva <guilherme.costa_EST@pampa.compasso>
2020-09-21 17:38:22 -0300

    Criado a rota de pegar cidades e usuários, a de delete também foi criado porém está em desenvolvimento, falta a confirmação do delete

--- routes.ts
+++ routes.ts
@@ -44,12 +44,48 @@ routes.post('/usuario', async (req, res) => {
 });
 
 routes.get('/cidade', async (req, res) => {
+  let buscaCidade = "";
+  if (req.query.nomeCidade) {
+    const nomeCidade = req.query.nomeCidade;
+    buscaCidade = await db('cidade').select('*').where('cidade.nome', '=', nomeCidade);
+  }
+  else if (req.query.nomeEstado) {
+    const nomeEstado = req.query.nomeEstado;
+    buscaCidade = await db('cidade').select('*').where('cidade.estado', '=', nomeEstado);
+  } else {
+    buscaCidade = res.status(400).send("Tente passar uma informação como nomeCidade ou nomeEstado");
+  }
 
-  const nome = req.query.nome as string;
 
-  const buscaCidade = await db('cidade').select('*').where('cidade.nome', '=', nome);
   console.log(buscaCidade);
   res.json(buscaCidade);
+});
+
+routes.get('/usuario', async (req, res) => {
+  let buscaUsuario;
+  const nomeUsuario = req.query.nomeUsuario;
+  const idUsuario = req.query.id;
+  if (nomeUsuario) {
+    buscaUsuario = await db('usuario').select('*').where('usuario.nome', '=', nomeUsuario);
+  } else if (idUsuario) {
+    buscaUsuario = await db('usuario').select('*').where('usuario.id', '=', idUsuario);
+  }
+  else {
+    buscaUsuario = res.status(400).send("Tente passar alguma informação como nomeUsuario ou id por exemplo");
+  }
+
+  res.send(buscaUsuario);
+});
+
+routes.delete('/usuario', async (req, res) => {
+  const idUsuario = req.query.id;
+  let deletaUsuario;
+  if (idUsuario) {
+    deletaUsuario = await db('usuario').delete('*').where('usuario.id', '=', idUsuario);
+  } else {
+    deletaUsuario = res.status(400).send('Tente preencher o campo id para deletar o usuário');
+  }
+  res.send(deletaUsuario);
 });
 
 export default routes;

3b539b894849809df44cef608ff6f6eeaa95a22b
Guilherme Costa da Silva <guilherme.costa_EST@pampa.compasso>
2020-10-07 19:20:54 -0300

    Criei o swagger, fiz funcionar e documentei tudo

--- server.ts
+++ server.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 import routes from './routes';
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('./swagger_output.json')
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(routes);
 

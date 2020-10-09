27cd92eaa01e2d68320f2cea41ed3a9722ef2e95
Guilherme Costa da Silva <guilherme.costa_EST@pampa.compasso>
2020-10-08 21:28:14 -0300

    Estrutura de pastas

--- typescript/server.ts
+++ typescript/server.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import routes from './routes';
-const swaggerUi = require('swagger-ui-express')
-const swaggerFile = require('./swagger_output.json')
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('./swagger_output.json');
 
 const app = express();
 
@@ -11,6 +11,4 @@ app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(routes);
 
-app.listen(3333, () => {
-  console.log("O servidor está rodando");
-});
+app.listen(3333, () => console.log("Servidor está rodando"));

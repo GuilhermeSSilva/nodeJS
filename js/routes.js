283ffccaa18b7cc2ae40d28c908b3ac7c01f6c2a
Guilherme Costa da Silva <guilherme.costa_EST@pampa.compasso>
2020-10-16 19:21:44 -0300

    Foi adicionado a rota de login, já está funcionando perfeitamente porém falta a implementação do token

--- js/routes.js
+++ js/routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const db = require('./database/connection.js');
-
+const { sha512, gerarSenha } = require('./utils/salt');
 const routes = express.Router();
 
 routes.post('/cidade', async (req, res) => {
@@ -14,28 +14,38 @@ routes.post('/cidade', async (req, res) => {
     estado
   })
     .then(() => res.status(200).send('cidade incluída com sucesso'))
-    .catch(() => res.status(400).send('A cidade não pode ser inserida'));
+    .catch(() => res.status(400).send('A cidade não pode ser inserida, tente preencher os campos nome e estado corretamente.'));
 });
 
 routes.post('/usuario', async (req, res) => {
 
   const {
     nome,
+    email,
+    senha,
     sexo,
     data_de_nascimento,
     idade,
     cidade
   } = req.body;
 
+  const novaSenha = gerarSenha(senha);
+  const {
+    salt,
+    hash
+  } = novaSenha;
   const insereUsuario = await db('usuario').insert({
     nome,
+    email,
+    salt,
+    hash,
     sexo,
     data_de_nascimento,
     idade,
     cidade
   })
     .then(() => res.status(200).send('Usuário inserido com sucesso!'))
-    .catch(() => res.status(400).send('Não foi possível inserir o usuário'));
+    .catch(() => res.status(400).send('Não foi possível inserir o usuário, tente preencher os campos nome, email, senha, sexo, data_de_nascimento, idade e cidade corretamente.'));
 
   res.send();
 });
@@ -99,6 +109,27 @@ routes.patch('/usuario', async (req, res) => {
       .catch(() => res.status(400).send("Não foi possível editar o nome do usuário"));
   } else {
     res.status(400).send('Preencha o id e nome do usuário corretamente');
+  }
+});
+
+routes.post('/usuario/login', async (req, res) => {
+  const {
+    email,
+    senha
+  } = req.body;
+  if (!email || !senha) {
+    res.send("Por favor preencha o campo e-mail e senha corretamnete.");
+  } else {
+    salt = await db('usuario').select('salt').where('usuario.email', '=', email);
+    if (salt) {
+      senhaUsuario = sha512(senha, salt[0].salt);
+      login = await db('usuario').select('*').where('hash', '=', senhaUsuario.hash)
+      if (login == "") {
+        res.send("Email ou senha incorretos.");
+      } else {
+        res.send("Logado com sucesso");
+      }
+    }
   }
 });
 
